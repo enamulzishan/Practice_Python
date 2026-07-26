7054a786bbd27a07f66b0777f2c79e1eae190699
enamulzishan <enamulzishan@users.noreply.github.com>
2026-07-26 23:51:30 +0600

    Add factorial calculator script

---
+++ Factorial.py
@@ -0,0 +1,9 @@
+print("Factorial number calculator")
+
+a = int(input("Enter a number: "))
+factorial = 1
+
+for i in range(1, a + 1):
+    factorial *= i
+
+print(f"The factorial of {a} is {factorial}")

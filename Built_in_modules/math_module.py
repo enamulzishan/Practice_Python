e125e0563cb7e84678e4144b25deb723d7d18113
enamulzishan <enamulzishan@users.noreply.github.com>
2026-08-04 01:38:17 +0600

    Add built-in module examples

---
+++ Built_in_modules/math_module.py
@@ -0,0 +1,19 @@
+import math
+print(math.pi)
+print(math.pow(2,3))
+print(math.sqrt(5))
+
+# rounding
+print(math.ceil(4.2))
+print(math.floor(4.8))
+print(math.trunc(4.9))
+
+print(math.fabs(-10))
+
+print(math.factorial(5))
+print(math.cos(0))
+print(math.sin(0))
+print(math.tan(0))
+
+print(math.log10(100))
+print(math.log(10))

a2e8b351527c4306e84937dcab106a653cfc8bdb
enamulzishan <enamulzishan@users.noreply.github.com>
2026-08-07 00:41:15 +0600

    Add password generator demo

--- Built_in_modules/random_demo.py
+++ Built_in_modules/random_demo.py
@@ -1,2 +1,8 @@
+print("Welcome to my Password generator")
 import random
-print(random.random())
+
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-+"
+password = ""
+for _ in range(8):
+    password += random.choice(characters)
+print("Your generated password is:", password)

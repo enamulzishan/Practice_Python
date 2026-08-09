4c010cd5e81341b9082388116657cda54f81cea1
enamulzishan <enamulzishan@users.noreply.github.com>
2026-08-10 00:03:44 +0600

    Update module-11-file-manager implementation and data files

--- module-11-file-manager/file_manager.py
+++ module-11-file-manager/file_manager.py
@@ -1,8 +1,10 @@
 print("Welcome to Smart File-Based Manager")
 
 import random
+import os
 import csv
 import datetime
+
 while True:
   print("\n1.Add new expense")
   print("2.View all expenses")
@@ -10,29 +12,51 @@ while True:
   print("4.View all notes")
   print("5.Exit")
   choice = input("Enter your choice: ")
+
   if choice == '1':
     expense = input("Enter expense title: ")
     amount = float(input("Enter expense amount: "))
+
     with open("E:\\OSTAD\\module-11-file-manager\\expenses.csv", "a", newline="") as file:
         writer = csv.writer(file)
         writer.writerow([random.randint(1, 1000), expense, amount, datetime.datetime.now()])
 
   elif choice == '2':
-    with open("E:\\OSTAD\\module-11-file-manager\\expenses.csv", "r") as file:
+    if os.path.exists("E:\\OSTAD\\module-11-file-manager\\expenses.csv"):
+      with open("E:\\OSTAD\\module-11-file-manager\\expenses.csv", "r") as file:
         reader = csv.reader(file)
+        found = False
+
         for row in reader:
-            print(row)
+          found = True
+          print(f"ID: {row[0]} | Title: {row[1]} | Amount: {row[2]} | Date: {row[3]}")
+
+        if not found:
+          print("No records found yet.")
+    else:
+      print("No records found yet.")
 
   elif choice == '3':
     note = input("Enter note title: ")
     content = input("Enter note content: ")
-    with open("E:\\OSTAD\\module-11-file-manager\\notes.txt", "a", newline="") as file:
+
+    with open("E:\\OSTAD\\module-11-file-manager\\notes.txt", "a") as file:
         file.write(f"{random.randint(1, 1000)},{note},{content},{datetime.datetime.now()}\n")
 
   elif choice == '4':
-    with open("E:\\OSTAD\\module-11-file-manager\\notes.txt", "r") as file:
+    if os.path.exists("E:\\OSTAD\\module-11-file-manager\\notes.txt"):
+      with open("E:\\OSTAD\\module-11-file-manager\\notes.txt", "r") as file:
+        found = False
+
         for line in file:
-            print(line.strip())
+          found = True
+          row = line.strip().split(",")
+          print(f"ID: {row[0]} | Title: {row[1]} | Content: {row[2]} | Date: {row[3]}")
+
+        if not found:
+          print("No records found yet.")
+    else:
+      print("No records found yet.")
 
   elif choice == '5':
     break

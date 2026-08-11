69d002bb082362a710b020ee186f5d938b3695ac
enamulzishan <enamulzishan@users.noreply.github.com>
2026-08-12 00:17:32 +0600

    Add LogicOperation.py

---
+++ LogicOperation.py
@@ -0,0 +1,44 @@
+print("welcome to my logic operation")
+
+# AND operation
+x = True
+y = False
+result = x and y
+print(result)
+
+# OR operation
+x = True
+y = False
+result = x or y
+print(result)
+
+# NOT operation
+x = True
+result = not x
+print(result)
+
+# XOR operation
+x = True
+y = False
+result = x != y
+print(result)
+
+# NAND operation
+x = True
+y = True
+result = not (x and y)
+print(result)
+
+# NOR operation
+x = False
+y = False
+result = not (x or y)
+print(result)
+
+# XNOR operation
+x = True
+y = False
+result = x == y
+print(result)
+
+
